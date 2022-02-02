4349b08482f00e485054fac3ea74d7f5edcd2ce9
William Merfalen <wmerfalen@gmail.com>
2022-02-01 21:27:39 -0700

    wip: need to do recipe pug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+	let div_class = '';
+
+	/**
+	 * Check if the div should have a specific css class
+	 */
+	if(req.query.error_class){
+		div_class = req.query.error_class;
+	}
+
+  res.render('login', {message: req.query.message, div_class });
 });
+
+router.post('/login', function(req, res, next) {
+	if(req.body.user === 'admin' && req.body.password === 'password') {
+		res.send('Login success');
+		return;
+	}
+	res.redirect(`/users/login?` + 
+		`message=user+${req.body.user}+login+failed` + 
+		`&error_class=error`
+	);
+	return;
+});
+
 
 module.exports = router;
